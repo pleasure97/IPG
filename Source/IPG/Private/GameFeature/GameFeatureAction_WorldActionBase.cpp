b419f71ee1c9ee6abd2c596126f179db155991d1
conple21 <conple21@gmail.com>
2026-04-28 10:30:04 +0900

    260428 (2) Initialized Game Feature Plugin and Made Game Feature Examples

---
+++ Source/IPG/Private/GameFeature/GameFeatureAction_WorldActionBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameFeature/GameFeatureAction_WorldActionBase.h"
+
+
+
+
---
+++ Source/IPG/Public/GameFeature/GameFeatureAction_WorldActionBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeatureAction.h"
+#include "GameFeatureAction_WorldActionBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IPG_API UGameFeatureAction_WorldActionBase : public UGameFeatureAction
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
