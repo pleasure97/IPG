e17bdcdcf057968e5532bfc642b9ffcb8d4cef1f
conple21 <conple21@gmail.com>
2026-06-22 14:39:01 +0900

    260622 (10) Initialized IPG State Tree Plugin

---
+++ Plugins/IPGStateTree/Source/IPGStateTree/IPGStateTree.Build.cs
@@ -0,0 +1,57 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class IPGStateTree : ModuleRules
+{
+	public IPGStateTree(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"EnhancedInput",
+                "AIModule",          // Required for AI State Trees
+				"StateTreeModule",   // Required for the State Tree plugin
+				"GameplayStateTreeModule" // Required for gameplay-specific State Tree tasks/conditions
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ Plugins/IPGStateTree/Source/IPGStateTree/Private/IPGStateTreeBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IPGStateTreeBPLibrary.h"
+#include "IPGStateTree.h"
+
+UIPGStateTreeBPLibrary::UIPGStateTreeBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UIPGStateTreeBPLibrary::IPGStateTreeSampleFunction(float Param)
+{
+	return -1;
+}
+
